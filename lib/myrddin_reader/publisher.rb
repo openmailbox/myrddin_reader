d2cdb11cacc50cd7607ce94ba9842ccaf407c16e
Brandon Rice <brice84@gmail.com>
2013-01-13 21:25:14 -0500

    Add specs for publisher.

--- lib/myrddin_reader/publisher.rb
+++ lib/myrddin_reader/publisher.rb
@@ -10,17 +10,21 @@ module MyrddinReader
              )
     end
 
-    private_class_method :new_bot
+    def self.build_routes
+      Index.new(@@bot.bb_list).boards.each do |board|
+        get "/#{board.title.downcase.gsub(' ','_')}" do
+          index = Index.new(@@bot.bb_list)
+          @board = index.boards.find { |i| i.title == board.title }
+          haml :board_index
+        end
+      end
+    end
+
+    private_class_method :new_bot, :build_routes
 
     @@bot = new_bot
 
-    Index.new(@@bot.bb_list).boards.each do |board|
-      get "/#{board.title.downcase.gsub(' ','_')}" do
-        index = Index.new(@@bot.bb_list)
-        @board = index.boards.find { |i| i.title == board.title }
-        haml :board_index
-      end
-    end
+    build_routes
 
     use Rack::Auth::Basic do |username, password|
       username == CONFIG['web_username'] and password == CONFIG['web_password']
@@ -34,5 +38,7 @@ module MyrddinReader
       @index = Index.new(@@bot.bb_list)
       haml :index
     end
-  end
+
+    
+  end 
 end
--- spec/myrddin_reader/publisher_spec.rb
+++ spec/myrddin_reader/publisher_spec.rb
@@ -2,5 +2,27 @@ require 'spec_helper'
 
 module MyrddinReader
   describe Publisher do
+    before(:all) do
+      Publisher.class_exec { @routes = {'GET' => [], 'HEAD' => []} }
+    end
+
+    let(:bot) { double('Bot') }
+    let(:bb_list) { File.open('spec/support/bblist.txt') { |i| i.readlines }.join }
+
+    before(:each) do
+      Publisher.class_variable_set(:@@bot, bot)
+    end
+
+    it 'has a class accessible bot' do
+      Publisher.class_variable_get(:@@bot).should_not be_nil
+    end
+
+    it 'builds a list of routes' do
+      bot.stub(:bb_list).and_return(bb_list)
+      lambda {
+        Publisher.class_eval('build_routes')
+      }.should change { Publisher.routes['GET'].count }.by(3)
+    end
+    
   end
 end
